a0e505aee6237f8e2b7096cbc226aed1c8247a71
Steve Beck <beck.steve@abc.net.au>
2017-05-09 11:00:22 +1000

    Added gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var sftp = require('gulp-sftp');
 var path = require('path');
 var fs = require('fs');
 
-gulp.task('less', function () {
-	fs.stat('./src/css/app.less', function(err, stat) {
-		if(err != null) {
-			console.log('Error:' + err.code);
-		}
-	});
-	gulp.src('./src/css/app.less')
-	.pipe(less({
-		paths: [ path.join(__dirname, 'less', 'includes') ]
-	}))
-	.pipe(gulp.dest('./src/css/'));
+gulp.task('less', function() {
+    fs.stat('./src/css/app.less', function(err, stat) {
+        if (err != null) {
+            console.log('Error:' + err.code);
+        }
+    });
+    gulp.src('./src/css/app.less')
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.task('default', function () {
-	gulp.watch('./src/css/*.less', ['less']);
+gulp.task('default', function() {
+    gulp.watch('./src/css/*.less', ['less']);
+});
+
+gulp.task('default', function() {
+    return gulp.src('src/*')
+        .pipe(sftp({
+            host: 'abc-shop-online.neto.com.au',
+            auth: 'keyMain',
+            port: 2022,
+            src: 'src',
+            dest: '/httpdocs/assets/themes/abcshop'
+        }));
 });
